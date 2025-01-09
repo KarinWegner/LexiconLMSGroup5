b1a556f4424842add09fa37c74e2fab0d0213ab6
KarinWegner <karinwegner00@gmail.com>
2025-01-09 15:11:23 +0100

    Added DTOs

---
+++ LMS.Shared/DTOs/ApplicationUserDTO.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LMS.Shared.DTOs
+{
+    public record ApplicationUserDTO
+    {
+        public string? Id { get; init; }
+        public string? UserName { get; init; }
+        public string? Name { get; init; }
+        public string? Email { get; init; }
+
+    }
+}
---
+++ LMS.Shared/DTOs/CourseDTO.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LMS.Shared.DTOs
+{
+    public record CourseDTO
+    {
+        public int Id { get; init; }
+        public string? Name { get; set; }
+        public string Description { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public IEnumerable<ApplicationUser> Enrollments { get; set; }
+    }
+}
