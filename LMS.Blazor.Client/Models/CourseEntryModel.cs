2e37d17eced46b844cd0046565d1804e4d8ea3b0
SinonSoderlund <sinonlynx@gmail.com>
2025-01-09 09:59:27 +0100

    Adding basic support classes for class entry display

---
+++ LMS.Blazor.Client/Models/CourseEntryModel.cs
@@ -0,0 +1,10 @@
+﻿using LMS.Shared.DTOs;
+
+namespace LMS.Blazor.Client.Models
+{
+    public class CourseEntryModel
+    {
+
+        public IEnumerable<CourseEntryDto> CourseEntries { get; set; }
+    }
+}
---
+++ LMS.Blazor.Client/Pages/Components/CourseEntryList.razor.cs
@@ -0,0 +1,11 @@
+﻿using LMS.Shared.DTOs;
+using Microsoft.AspNetCore.Components;
+
+namespace LMS.Blazor.Client.Pages.Components
+{
+    public partial class CourseEntryList
+    {
+        [Parameter]
+        public CourseEntryDto[] ListEntries { get; set; }
+    }
+}
