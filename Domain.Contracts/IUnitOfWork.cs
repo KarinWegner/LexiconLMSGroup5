0a823f91873df7d5cfb59352a8f91ca30ff6e76e
Ivona Josipovic <josipovic.ivona@gmail.com>
2025-01-14 14:41:02 +0100

    UOW and GenericRepository

--- Domain.Contracts/IUnitOfWork.cs
+++ Domain.Contracts/IUnitOfWork.cs
@@ -1,6 +1,12 @@
-﻿namespace Domain.Contracts;
+﻿using Domain.Models.Entities;
+
+namespace Domain.Contracts;
 
 public interface IUnitOfWork
 {
+    IGenericRepository<Course> Courses { get; }
+    IGenericRepository<Module> Modules { get; }
+    IGenericRepository<Activity> Activities { get; }
+    IGenericRepository<Document> Documents { get; }
     Task CompleteASync();
 }
--- LMS.Infrastructure/Repositories/UnitOfWork.cs
+++ LMS.Infrastructure/Repositories/UnitOfWork.cs
@@ -1,19 +1,30 @@
 ﻿using Domain.Contracts;
+using Domain.Models.Entities;
 using LMS.Infrastructure.Data;
 
 namespace LMS.Infrastructure.Repositories;
 
 public class UnitOfWork : IUnitOfWork
 {
-    private readonly LmsContext context;
+    private readonly LmsContext _context;
+
+    private IGenericRepository<Course> _courses;
+    private IGenericRepository<Module> _modules;
+    private IGenericRepository<Activity> _activities;
+    private IGenericRepository<Document> _documents;
 
     public UnitOfWork(LmsContext context)
     {
-        this.context = context;
+        _context = context;
     }
+
+    public IGenericRepository<Course> Courses => _courses ??= new GenericRepository<Course>(_context);
+    public IGenericRepository<Module> Modules => _modules ??= new GenericRepository<Module>(_context);
+    public IGenericRepository<Activity> Activities => _activities ??= new GenericRepository<Activity>(_context);
+    public IGenericRepository<Document> Documents => _documents ??= new GenericRepository<Document>(_context);
 
     public async Task CompleteASync()
     {
-        await context.SaveChangesAsync();
+        await _context.SaveChangesAsync();
     }
 }
