61add54a8a19defe28ef9cbdf7bcae5ac056e69d
Svitlana Rybakova <rsv0011@gmail.com>
2025-01-16 18:40:59 +0100

    feat: add the new generic CallApiAsync

--- LMS.Blazor.Client/Services/IApiService.cs
+++ LMS.Blazor.Client/Services/IApiService.cs
@@ -5,5 +5,6 @@ namespace LMS.Blazor.Client.Services;
 
 public interface IApiService
 {
-    Task<IEnumerable<DemoDto>> CallApiAsync();
+    Task<TResponse?> GetAsync<TResponse>(string endpoint);
+    Task<TResponse?> PostAsync<TRequest, TResponse>(string endpoint, TRequest dto);
 }
