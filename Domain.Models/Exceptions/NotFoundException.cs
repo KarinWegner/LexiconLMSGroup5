199a8d9e10a432d0f3e76976e1647d19c136ba53
KarinWegner <karinwegner00@gmail.com>
2025-01-20 10:36:33 +0100

    Added NotFoundExceptions

---
+++ Domain.Models/Exceptions/NotFoundException.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models.Exceptions
+{
+    public abstract class NotFoundException : Exception
+    {
+        public string Title { get; set; }
+        protected NotFoundException(string message, string title = "Not Found") : base(message)
+        {
+            Title = title;
+        }
+    }
+    public class CourseNotFoundException : NotFoundException
+    {
+        public CourseNotFoundException(int id) : base($"The Course with {id} is not found")
+        {
+
+        }
+    }
+    public class ModuleNotFoundException : NotFoundException
+    {
+        public ModuleNotFoundException(int id) : base($"The Module with {id} is not found")
+        {
+
+        }
+    }
+    public class ActivityNotFoundException : NotFoundException
+    {
+        public ActivityNotFoundException(int id) : base($"The Activity with {id} is not found")
+        {
+
+        }
+    }
+}
---
+++ LMS.API/Extensions/ExceptionMiddleWare.cs
@@ -0,0 +1,67 @@
+﻿using Domain.Models.Exceptions;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+
+namespace LMS.API.Extensions
+{
+    public static class ExceptionMiddleWare
+    {
+        public static void ConfigureExceptionHandler(this WebApplication app)
+        {
+            app.UseExceptionHandler(appError =>
+            {
+                appError.Run(async context =>
+                {
+
+                    var contextFeatures = context.Features.Get<IExceptionHandlerFeature>();
+                    if (contextFeatures != null)
+                    {
+                        var problemDetailsFactory = app.Services.GetService<ProblemDetailsFactory>();
+                        //Validate
+
+                        var problemDetails = CreateProblemDetails(context, contextFeatures.Error, problemDetailsFactory, app);
+
+                        context.Response.StatusCode = problemDetails.Status ?? StatusCodes.Status500InternalServerError;
+                        //context.Response.ContentType= ""
+                        await context.Response.WriteAsJsonAsync(problemDetails);
+                    }
+
+                });
+            });
+        }
+        private static ProblemDetails CreateProblemDetails(HttpContext context, Exception error, ProblemDetailsFactory? problemDetailsFactory, WebApplication app)
+        {
+            return error switch
+            {
+                CourseNotFoundException courseNotFoundException => problemDetailsFactory.CreateProblemDetails(
+                    context,
+                    StatusCodes.Status404NotFound,
+                    title: courseNotFoundException.Title,
+                    detail: courseNotFoundException.Message,
+                    instance: context.Request.Path),
+
+                ModuleNotFoundException moduleNotFoundException => problemDetailsFactory.CreateProblemDetails(
+                context,
+                StatusCodes.Status404NotFound,
+                title: moduleNotFoundException.Title,
+                detail: moduleNotFoundException.Message,
+                instance: context.Request.Path),
+
+                ActivityNotFoundException activityNotFoundException => problemDetailsFactory.CreateProblemDetails(
+                    context,
+                    StatusCodes.Status404NotFound,
+                    title: activityNotFoundException.Title,
+                    detail: activityNotFoundException.Message,
+                    instance: context.Request.Path),
+
+                _ => problemDetailsFactory.CreateProblemDetails(
+                 context,
+                 StatusCodes.Status500InternalServerError,
+                 title: "Internal Server Error",
+                 detail: app.Environment.IsDevelopment() ? error.Message : "An unexpected error occurred.")
+            };
+
+        }
+    }
+}
