7c109e80208f1d4425f9e49cf63f907bdd6e4b77
Ivona Josipovic <josipovic.ivona@gmail.com>
2025-01-22 10:41:58 +0100

    CourseParticipantDO inherits role from ApplicationUserDTO

--- LMS.Shared/DTOs/CourseParticipantDO.cs
+++ LMS.Shared/DTOs/CourseParticipantDO.cs
@@ -10,13 +10,10 @@ namespace LMS.Shared.DTOs
     public record CourseParticipantDO : ApplicationUserDTO
     {
         public string? ProfilePictureLink { get; set; }
-        public string? Role { get; set; }
 
-        public CourseParticipantDO(string? ProfilePictureLink, string? role, string? id, string? username, string? name, string? email) : base(id, username, name, email)
+        public CourseParticipantDO(string? ProfilePictureLink, string? role, string? id, string? username, string? name, string? email) : base(id, username, name, email, role)
         {
-
             this.ProfilePictureLink = ProfilePictureLink ?? $"https://picsum.photos/{new Random().Next(10,1000)}";
-            Role = role;
         }
 
         public CourseParticipantDO() { }
