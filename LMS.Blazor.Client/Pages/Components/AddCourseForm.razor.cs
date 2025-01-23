e0b2c91e6ccc7fe39902bb991a52bc24208bb568
Ivona Josipovic <josipovic.ivona@gmail.com>
2025-01-23 14:31:30 +0100

    Create course progress

---
+++ LMS.Blazor.Client/Pages/Components/AddCourseForm.razor.cs
@@ -0,0 +1,18 @@
+﻿using LMS.Blazor.Client.Services;
+using Microsoft.AspNetCore.Components;
+using LMS.Shared.DTOs.CourseDTOs;
+
+namespace LMS.Blazor.Client.Pages.Components
+{
+    public partial class AddCourseForm
+    {
+        protected override async Task OnInitializedAsync()
+        {
+            base.OnInitialized();
+            Model = new CourseCreateDTO();
+        }
+    }
+}
+
+
+
