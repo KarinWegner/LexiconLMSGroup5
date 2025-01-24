0e538ff426fec6081d7997a8662733e6ac181c31
KarinWegner <karinwegner00@gmail.com>
2025-01-24 09:44:11 +0100

    Fixed pagination

--- LMS.Services/EnrollmentService.cs
+++ LMS.Services/EnrollmentService.cs
@@ -206,9 +206,8 @@ namespace LMS.Services
 
         public async Task<ApiBaseResponse> GetUsers(string? roleFilter, int pageNr = 1, int pageSize=1)
         {
-            IQueryable<ApplicationUser> query = _uow.Courses.Query()                                                              
-                                                              .Include(c => c.Enrollments)
-                                                              .SelectMany(c => c.Enrollments);
+            IQueryable<ApplicationUser> query = _uow.Enrollments.UserQuery()
+                                                              .Where(u => u.Role == roleFilter);
             int totalCount = query.Count();
 
             var userList = await query
